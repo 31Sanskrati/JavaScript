6d52febe2cff44a1cd06f413cdb11f150e1ac369
Sanskrati <31sanskrati@gmail.com>
2021-09-07 22:08:43 +0530

    Add reversing an array solution

---
+++ Chapter 4/Exercise/reversing an array.js
@@ -0,0 +1,19 @@
+function reverseArray(array){
+    let newArray = [];
+    for(let i = array.length-1; i>=0; i--){
+        newArray.push(array[i]);
+    }
+    return newArray;
+}
+
+function reverseArrayInPlace(array){
+    for(let i = 0; i<array.length/2; i++){
+        let temp = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = temp;
+    }
+    return array;
+}
+
+console.log(reverseArray([1,2,3,4,5])); //[ 5, 4, 3, 2, 1 ]
+console.log(reverseArrayInPlace([1,2,3,4,5])); //[ 5, 4, 3, 2, 1 ]
